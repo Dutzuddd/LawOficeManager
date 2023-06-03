6ec4c972d2591bbef93956ba24ac654e35e0d435
Dutzuddd <vladsfranciog@gmail.com>
2023-06-03 10:15:16 +0300

    inintial commit - Registration and Login

---
+++ LicentaSfranciog/Areas/Identity/Data/UserDbContext.cs
@@ -0,0 +1,34 @@
+﻿using LicentaSfranciog.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LicentaSfranciog.Areas.Identity.Data;
+
+public class UserDbContext : IdentityDbContext<ApplicationUser>
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Adding custom registration
+        builder.ApplyConfiguration(new ApplicationUserEntity());
+
+    }
+}
+
+internal class ApplicationUserEntity : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+       // Max. character length for Nume and Prenume
+       builder.Property(u  => u.Nume).HasMaxLength(35);
+       builder.Property(u => u.Prenume).HasMaxLength(35);
+    }
+}
---
+++ LicentaSfranciog/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LicentaSfranciog.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("UserDbContextConnection") ?? throw new InvalidOperationException("Connection string 'UserDbContextConnection' not found.");
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<UserDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Adding Razor Pages
+app.MapRazorPages();
+
+
+app.Run();
